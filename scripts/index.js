e143c442bbf040e6ddbf797373c7d28fb307c7b2
Muadh Ghuneim <muaeim@gmail.com>
2018-02-06 17:05:42 -0500

    Made navbar switch pages on clicking button

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,44 @@
 (function () {
+    // globals
+    var selectedTab = $("#profile-tab");
+    var selectedPage = $("#profile-page");
 
-    
+    // Hide all pages by default
+    var pages = $("#pages").children();
+    for(var p = 0; p < pages.length; p++) {
+        var page = $(pages[p]);
+        if(page.attr("id") != selectedPage.attr("id")) {
+            page.hide();
+        }
+    }
+
+    // Add action listener to navbar
+    $("#tabs").click(function(event) {
+        if(event.target !== event.currentTarget) {
+            switchTabs($(event.target).parent());
+        }
+    });
+
+    // Strip the name from object ids
+    function getName(hyphenatedObject) {
+        var name = $(hyphenatedObject).attr("id");
+        return name.split("-")[0];
+    }
+
+    // Switch tabs in the navbar
+    function switchTabs(tab) {
+        var name = getName(tab);
+        var nameTab = $("#" + name + "-tab");
+        var namePage = $("#" + name + "-page");
+
+        selectedTab.removeClass("active");
+        nameTab.addClass("active");
+
+        selectedPage.hide();
+        namePage.show();
+
+        selectedTab = nameTab;
+        selectedPage = namePage;
+    }
+
 })();
